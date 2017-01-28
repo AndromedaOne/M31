6f068e51ed086d5ab32905155fcb7ff6db1c2e6c
FRC4905 <frc4905software@gmail.com>
2017-01-28 12:55:40 -0500

    work in progress shooter code

--- eclipse/M31/src/org/usfirst/frc4905/M31/commands/getEncPos.java
+++ eclipse/M31/src/org/usfirst/frc4905/M31/commands/getEncPos.java
@@ -41,7 +41,7 @@ public class getEncPos extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.driveTrain.getEncPos();
+    	//Robot.driveTrain.getEncPos();
     }
 
     // Make this return true when this Command no longer needs to run execute()
