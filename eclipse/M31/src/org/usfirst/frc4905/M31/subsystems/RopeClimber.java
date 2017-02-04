6a26b17733ab67306f35b642ab2ffe776fbafcda
danamon2002 <danamon2002@a62ca21b-6cca-69b1-290b-ce57b7db7bff>
2017-02-04 16:57:17 +0000

    Rope Climber Code
    First Commit
    
    git-svn-id: https://github.com/AndromedaOne/M31.git/trunk@79 a62ca21b-6cca-69b1-290b-ce57b7db7bff

---
+++ eclipse/M31/src/org/usfirst/frc4905/M31/subsystems/RopeClimber.java
@@ -0,0 +1,29 @@
+package org.usfirst.frc4905.M31.subsystems;
+
+import org.usfirst.frc4905.M31.RobotMap;
+import org.usfirst.frc4905.M31.commands.climbRope;
+
+import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+
+/**
+ *
+ */
+public class RopeClimber extends Subsystem {
+
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+	//declarations
+	public VictorSP ropeMotor;
+	public void climbRope() {
+		ropeMotor.setSpeed(0.2);
+		
+	}
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    	setDefaultCommand(new climbRope());
+    }
+}
+
