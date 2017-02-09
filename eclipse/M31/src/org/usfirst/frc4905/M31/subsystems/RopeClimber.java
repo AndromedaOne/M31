932aa083872e0ec6b2dc92a74618ed12c373046f
wjd14 <will@ayerdoyles.org>
2017-02-08 19:36:23 -0500

    added code to stop the climber

--- eclipse/M31/src/org/usfirst/frc4905/M31/subsystems/RopeClimber.java
+++ eclipse/M31/src/org/usfirst/frc4905/M31/subsystems/RopeClimber.java
@@ -20,6 +20,12 @@ public class RopeClimber extends Subsystem {
 		ropeMotor.setSpeed(0.2);
 		
 	}
+	
+	public void stopClimber(){
+		ropeMotor.setSpeed(0);
+		
+	}
+	
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
