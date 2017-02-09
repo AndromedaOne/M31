3916b41d6cb089e8823d131597ed635ed5b578be
FRC4905 <frc4905software@gmail.com>
2017-02-08 19:11:23 -0500

    Basic Autonomous Code (Boiler)

---
+++ eclipse/M31/src/org/usfirst/frc4905/M31/commands/MoveY.java
@@ -0,0 +1,52 @@
+package org.usfirst.frc4905.M31.commands;
+
+import org.usfirst.frc4905.M31.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class MoveY extends Command {
+	
+	private double m_distance = 0;
+
+    public MoveY() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(Robot.driveTrain);
+    }
+    
+    public MoveY(double distance) {
+    	
+    	requires (Robot.driveTrain);
+    	m_distance = distance;
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	
+    	Robot.driveTrain.moveToYEncoderRevolutions(m_distance);
+    	
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return Robot.driveTrain.isDoneMovingToYEncoderPosition();
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	Robot.driveTrain.stopMovingToYEncoderRevolutions();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	end();
+    }
+}
