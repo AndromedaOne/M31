8814528af1935f496d1b66f9361bd15778b36f46
otmas <otmas@a62ca21b-6cca-69b1-290b-ce57b7db7bff>
2017-02-11 15:27:18 +0000

    Fix Method Names
    git-svn-id: https://github.com/AndromedaOne/M31.git/trunk@63 a62ca21b-6cca-69b1-290b-ce57b7db7bff

--- eclipse/M31/src/org/usfirst/frc4905/M31/commands/MoveY.java
+++ eclipse/M31/src/org/usfirst/frc4905/M31/commands/MoveY.java
@@ -36,7 +36,7 @@ public class MoveY extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return Robot.driveTrain.isDoneMovingToYEncoderPosition();
+        return Robot.driveTrain.isDoneMovingToYEncoderRevolutions();
     }
 
     // Called once after isFinished returns true
