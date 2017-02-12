d71fa33539017613b610a0822a384a3ba1afd145
FRC4905 <frc4905software@gmail.com>
2017-02-11 19:30:24 -0500

    wip gearhandler

--- eclipse/M31/src/org/usfirst/frc4905/M31/commands/MoveLeftGearHandler.java
+++ eclipse/M31/src/org/usfirst/frc4905/M31/commands/MoveLeftGearHandler.java
@@ -7,12 +7,14 @@ import edu.wpi.first.wpilibj.command.Command;
 /**
  *
  */
-public class closeGearHandler extends Command {
-
-    public closeGearHandler() {
+public class MoveLeftGearHandler extends Command {
+	private double m_speed = 0;
+    public MoveLeftGearHandler(double speed) {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     	requires(Robot.gearHandler);
+    	m_speed = speed;
+    	
     }
 
     // Called just before this Command runs the first time
@@ -21,20 +23,22 @@ public class closeGearHandler extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.gearHandler.closeHandler();
+    	Robot.gearHandler.moveLeftGearHandler(m_speed);
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return Robot.gearHandler.isDoneClose();
+        return Robot.gearHandler.shouldStopMovingLeft();
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    	Robot.gearHandler.stopMovingLeft();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	end();
     }
 }
--- eclipse/M31/src/org/usfirst/frc4905/M31/commands/MoveRightGearHandler.java
+++ eclipse/M31/src/org/usfirst/frc4905/M31/commands/MoveRightGearHandler.java
@@ -7,34 +7,56 @@ import edu.wpi.first.wpilibj.command.Command;
 /**
  *
  */
-public class openGearHandler extends Command {
-
-    public openGearHandler() {
+public class MoveRightGearHandler extends Command {
+	private double m_speed;
+	private double counter = 0;
+	private boolean startConfig;
+    public MoveRightGearHandler(double speed) {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
     	requires(Robot.gearHandler);
+    	m_speed = speed;
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	startConfig = Robot.gearHandler.shouldStopMovingRight();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.gearHandler.openHandler();
+    	Robot.gearHandler.moveRightGearHandler(m_speed);
+    	
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return Robot.gearHandler.isDoneOpen();
+    	if(Robot.gearHandler.shouldStopMovingRight() == false){
+    		counter++;
+    		if(counter > 40){
+    			
+    			return true;
+    		}
+    		
+    	}else{
+    		counter = 0;
+    	}
+    	return false;
+    	 // return Robot.gearHandler.shouldStopMovingRight();
+      
+    	  
+       
+    	
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    	Robot.gearHandler.stopMovingRight();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	end();
     }
 }
