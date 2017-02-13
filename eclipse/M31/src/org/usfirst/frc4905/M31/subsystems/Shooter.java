e7487da5b668cd2837684b50534d19419e842075
frc4905 <frc4905@a62ca21b-6cca-69b1-290b-ce57b7db7bff>
2017-02-13 17:15:48 +0000

    wip shooter code
    git-svn-id: https://github.com/AndromedaOne/M31.git/trunk@89 a62ca21b-6cca-69b1-290b-ce57b7db7bff

---
+++ eclipse/M31/src/org/usfirst/frc4905/M31/subsystems/Shooter.java
@@ -0,0 +1,37 @@
+package org.usfirst.frc4905.M31.subsystems;
+
+import org.usfirst.frc4905.M31.RobotMap;
+
+import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class Shooter extends Subsystem {
+
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+	public VictorSP feederMotor = RobotMap.feederMotor;
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+    
+    
+    public static void spinFeederCCW(){
+    	
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
