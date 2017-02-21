8d17d01d1193dbab0178f8c2aeef6c352f104e91
william.j..schilp..phd <william.j..schilp..phd@a62ca21b-6cca-69b1-290b-ce57b7db7bff>
2017-02-21 16:03:41 +0000

    Merge branch 'master' into TraceUtility
    # Conflicts:
    #	eclipse/M31/src/org/usfirst/frc4905/M31/Robot.java
    #	eclipse/M31/src/org/usfirst/frc4905/M31/subsystems/DriveTrain.java
    
    
    git-svn-id: https://github.com/AndromedaOne/M31.git/trunk@173 a62ca21b-6cca-69b1-290b-ce57b7db7bff

--- eclipse/M31/src/org/usfirst/frc4905/M31/commands/PutTimeStampOnNetworkTables.java
+++ eclipse/M31/src/org/usfirst/frc4905/M31/commands/PutTimeStampOnNetworkTables.java
@@ -3,57 +3,37 @@ package org.usfirst.frc4905.M31.commands;
 import org.usfirst.frc4905.M31.Robot;
 
 import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  *
  */
-public class MoveY extends Command {
-	
-	private double m_distance = 0;
+public class PutTimeStampOnNetworkTables extends Command {
 
-    public MoveY() {
+    public PutTimeStampOnNetworkTables() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
-    	requires(Robot.driveTrain);
-    }
-    
-    public MoveY(double distance) {
-    	
-    	requires (Robot.driveTrain);
-    	setDistanceToMoveY(distance);
-    }
-    
-    public void setDistanceToMoveY(double distance) {
-    	m_distance = distance;
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	
-    	Robot.driveTrain.moveToYEncoderRevolutions(m_distance);
-    	
+    	Robot.visionProcessing.putTimestampOnNetworkTables();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	System.out.println("Error:" +Robot.driveTrain.getYPIDcontroller().getError());
-    	
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return Robot.driveTrain.isDoneMovingToYEncoderRevolutions();
+        return true;
     }
 
     // Called once after isFinished returns true
     protected void end() {
-    	Robot.driveTrain.stopMovingToYEncoderRevolutions();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
-    	end();
     }
 }
--- eclipse/M31/src/org/usfirst/frc4905/M31/commands/SetMoveXDistanceWithVision.java
+++ eclipse/M31/src/org/usfirst/frc4905/M31/commands/SetMoveXDistanceWithVision.java
@@ -3,57 +3,43 @@ package org.usfirst.frc4905.M31.commands;
 import org.usfirst.frc4905.M31.Robot;
 
 import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  *
  */
-public class MoveY extends Command {
+public class SetMoveXDistanceWithVision extends Command {
+	private MoveX m_moveX;
 	
-	private double m_distance = 0;
-
-    public MoveY() {
+    public SetMoveXDistanceWithVision(MoveX moveX) {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
-    	requires(Robot.driveTrain);
-    }
-    
-    public MoveY(double distance) {
-    	
-    	requires (Robot.driveTrain);
-    	setDistanceToMoveY(distance);
-    }
-    
-    public void setDistanceToMoveY(double distance) {
-    	m_distance = distance;
+    	m_moveX = moveX;
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
-    	
-    	Robot.driveTrain.moveToYEncoderRevolutions(m_distance);
-    	
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	System.out.println("Error:" +Robot.driveTrain.getYPIDcontroller().getError());
-    	
+    	double distance = 0;
+    	if (Robot.visionProcessing.getFoundLift()){
+    		distance = Robot.visionProcessing.getForwardDistance()*1; //add scale factor
+    	}
+    	m_moveX.setDistanceToMoveX(distance);
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return Robot.driveTrain.isDoneMovingToYEncoderRevolutions();
+        return true;
     }
 
     // Called once after isFinished returns true
     protected void end() {
-    	Robot.driveTrain.stopMovingToYEncoderRevolutions();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
-    	end();
     }
 }
