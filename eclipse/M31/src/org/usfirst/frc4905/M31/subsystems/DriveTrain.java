0e43aa30325b15798463ac14d5eb3895b1ba56e3
william.j..schilp..phd <william.j..schilp..phd@a62ca21b-6cca-69b1-290b-ce57b7db7bff>
2017-02-21 16:55:18 +0000

    Merge remote-tracking branch 'origin/VisionAutonomousRoutines' into TraceUtility
    # Conflicts:
    #	eclipse/M31/src/org/usfirst/frc4905/M31/subsystems/DriveTrain.java
    
    
    git-svn-id: https://github.com/AndromedaOne/M31.git/trunk@174 a62ca21b-6cca-69b1-290b-ce57b7db7bff

--- eclipse/M31/src/org/usfirst/frc4905/M31/subsystems/DriveTrain.java
+++ eclipse/M31/src/org/usfirst/frc4905/M31/subsystems/DriveTrain.java
@@ -168,7 +168,7 @@ public class DriveTrain extends Subsystem {
 
 		// Greatest Regards to 1519
 		// update count of iterations since rotation last commanded
-		/*if (gyroEnabled) {
+		if (gyroEnabled) {
 			if ((-0.01 < rotation) && (rotation < 0.01)) {
 				// rotation is practically zero, so just set it to zero and
 				// increment iterations
@@ -182,7 +182,7 @@ public class DriveTrain extends Subsystem {
 			if (m_iterationsSinceRotationCommanded == 20) {
 				m_desiredHeading = gyroReading;
 			} else if (m_iterationsSinceRotationCommanded > 20) {
-				//rotation = (m_desiredHeading - gyroReading) / 50.0;
+				rotation = (m_desiredHeading - gyroReading) / 50.0;
 			}
 		}
 		if(prefs.getBoolean("Mecanum Logging", false)) {
@@ -193,7 +193,7 @@ public class DriveTrain extends Subsystem {
 			SmartDashboard.putNumber("Y Commanded Speed",yIn);
 			SmartDashboard.putNumber("X Commanded Speed", xIn);
 			SmartDashboard.putNumber("Rotation", rotation);
-		}*/
+		}
 		Trace traceInst = Trace.getInstance();
 		Vector<Double> entry = new Vector<Double>();
 		entry.add(Robot.driveTrain.getM1Speed());
